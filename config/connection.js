6ed91ab549bcd980e586e43940b90a39f964f484
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-09-16 20:14:20 +0530

    refactor: refactor code at added callback for database connection successful

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 //Set up default mongoose connection
 
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true},() => { 
+	console.log("Database Connected Successfully"); 
+});
 
 //Get the default connection
 var db = mongoose.connection;
