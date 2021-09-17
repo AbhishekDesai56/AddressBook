e31ebbb913066e632ad78ef9bb3addb043c2f0a0
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-09-17 23:30:41 +0530

    refactor: added update method to update the addressbook data

--- app/controller/address.js
+++ app/controller/address.js
@@ -1,4 +1,5 @@
 const service = require('../service/address');
+const helper = require('../util/helper');
 
 class AddressBookController {
     saveAddressBookData = (req, res) => {
@@ -13,12 +14,12 @@ class AddressBookController {
 
         service.saveAddressBookDetails(addressBookData, (err, data) => {
             if (err) {
-                return res.status(400).json({
+                return res.status(helper.httpStatusCodeEnum.BAD_REQUEST).json({
                     success: false,
                     message: err.message
                 });
             } else {
-                return res.status(201).json({
+                return res.status(helper.httpStatusCodeEnum.CREATED).json({
                     success: true,
                     message: "Address Book Data has been save successfully",
                 });
@@ -29,12 +30,12 @@ class AddressBookController {
     getAddressBookData = (req, res) => {
         service.getAddressBookData((err, addressBookData) => {
           if (err) {
-            return res.status(400).send({
+            return res.status(helper.httpStatusCodeEnum.BAD_REQUEST).send({
               success: false,
               message: err.message
             });
           }
-          res.status(200).send({
+          res.status(helper.httpStatusCodeEnum.OK).send({
             success: true,
             message: 'Data is fetch successfully',
             data: addressBookData
@@ -46,17 +47,50 @@ class AddressBookController {
         const addressBookId = req.params.addressBookId;
         service.getAddressBookDataById(addressBookId, (err, addressBookData) => {
             if (err) {
-            return res.status(400).send({
+            return res.status(helper.httpStatusCodeEnum.BAD_REQUEST).send({
                 success: false,
                 message: err.message
             });
             }
-            res.status(200).send({
+            res.status(helper.httpStatusCodeEnum.OK).send({
             success: true,
             message: `Record is found with id ${addressBookId}`,
             data: addressBookData
             });
         });
+    }
+
+    updateAddressBookData = (req, res) => {
+        try {
+            const addressBookId = req.params.addressBookId;
+            const addressBookData = {
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                phoneNumber: req.body.phoneNumber,
+                city: req.body.city, 
+                pinCode: req.body.pinCode,
+                address: req.body.address,
+            };
+
+            service.updateAddressBookData(addressBookId,addressBookData, (err, addressBookData) => {
+                if (err) {
+                    return res.status(helper.httpStatusCodeEnum.BAD_REQUEST).send({
+                        success: false,
+                        message: err.message
+                    });
+                }
+                res.status(helper.httpStatusCodeEnum.OK).send({
+                success: true,
+                message: `Record updated successfully`
+                });
+            });
+        } 
+        catch(ex) {
+            res.status(helper.httpStatusCodeEnum.INTERNAL_SERVER).send({
+                success: false,
+                message: ex.message,
+            });
+        }
     }
 }
 
