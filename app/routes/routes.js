a619bee04cf23056a9c0eca284c32c19865cf9e7
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-09-17 23:34:09 +0530

    refactor: added route to update the addressbook data

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -4,4 +4,5 @@ module.exports = (app) => {
 	app.post("/saveAddressBookData", address.saveAddressBookData);
 	app.get("/getAddressBookData", address.getAddressBookData);
 	app.get("/getAddressBookData/:addressBookId", address.getAddressBookDataById);
+	app.put("/updateAddressBookData/:addressBookId", address.updateAddressBookData);
 };
