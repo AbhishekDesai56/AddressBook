09641e2781f22d419820c4015a221d22ca95d8a3
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-09-17 19:51:17 +0530

    refactor: added new routes to get addressbook data by id

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -3,4 +3,5 @@ const address = require("../controller/address");
 module.exports = (app) => {
 	app.post("/saveAddressBookData", address.saveAddressBookData);
 	app.get("/getAddressBookData", address.getAddressBookData);
+	app.get("/getAddressBookData/:addressBookId", address.getAddressBookDataById);
 };
