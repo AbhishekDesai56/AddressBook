a274f4c3040b7948e5988b69f54f2e98e19c07c0
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-09-17 19:48:56 +0530

    refactor: added new method to get the address book data by id

--- app/service/address.js
+++ app/service/address.js
@@ -12,6 +12,12 @@ class AddressBookService {
             return err ? addressBookDataCallback(err, null) : addressBookDataCallback(null, data);
         });
     }
+
+    getAddressBookDataById = (addressBookId, addressBookDataCallback) => {
+        model.getAddressBookDataById(addressBookId, (err, data) => {
+            return err ? addressBookDataCallback(err, null) : addressBookDataCallback(null, data);
+        });
+    }
 }
 
 module.exports = new AddressBookService();
