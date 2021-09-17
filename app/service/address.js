9e4f8257b2cb630159718248ed81672e66a6eacd
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-09-17 06:55:20 +0530

    refactor: return is not pass to function is corrected

--- app/service/address.js
+++ app/service/address.js
@@ -3,7 +3,7 @@ const model = require("../model/address");
 class AddressBookService {
     saveAddressBookDetails = (addressBookData, saveAddressBookDataCallback) => {
         model.saveAddressBookDetails(addressBookData, (err, data) => {
-            if (err) ? saveAddressBookDataCallback(err, null) : saveAddressBookDataCallback(err, data);
+            return (err) ? saveAddressBookDataCallback(err, null) : saveAddressBookDataCallback(err, data);
         });
     }
 }
