ba35c71af661240d6aff1393c5ceac8ca0aa7bb1
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-09-17 07:28:26 +0530

    feat: added routes to get all addressbok data

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -2,4 +2,5 @@ const address = require("../controller/address");
 
 module.exports = (app) => {
 	app.post("/saveAddressBookData", address.saveAddressBookData);
+	app.get("/getAddressBookData", address.getAddressBookData);
 };
