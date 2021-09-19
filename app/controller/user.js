839caee397628bef31153d4ccbeff787d1158fc9
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-09-20 01:15:50 +0530

    refactor: pass encrypt password to database

--- app/controller/user.js
+++ app/controller/user.js
@@ -2,12 +2,13 @@ const service = require('../service/user');
 const helper = require('../util/helper');
 
 class UserController {
-    saveUserData = (req, res) => {
+    saveUserData = async (req, res) => {
+        const hashedPassword = await helper.securePassword(req.body.password);
         const userData = {
             firstName: req.body.firstName,
             lastName: req.body.lastName,
             email: req.body.email,
-            password: req.body.password,
+            password: hashedPassword,
         };
 
         service.registerUser(userData, (err, data) => {
@@ -17,13 +18,30 @@ class UserController {
                     message: err.message
                 });
             } 
-            
+
             return res.status(helper.httpStatusCodeEnum.CREATED).json({
                 success: true,
                 message: "User Data has been save successfully",
             });
         });
     }
+
+    login = (req, res) => {
+        const loginDetails = {
+            email: req.body.email,
+            password: req.body.password,
+        };
+
+        service.loginUser(loginDetails, (err, data) => {
+        if (err) {
+            return res.status(helper.httpStatusCodeEnum.BAD_REQUEST).json({ success: false, message: err.message, data });
+        } 
+        return res.status(helper.httpStatusCodeEnum.OK).json({
+            success: true,
+            message: "User logged in successfully",
+        });
+        });
+    };
 }
 
 module.exports = new UserController();
