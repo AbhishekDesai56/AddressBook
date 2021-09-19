0a226332999a43eb28d2d1179307ca209a8a20d5
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-09-19 23:34:54 +0530

    refactor: added correct service method name

--- app/controller/user.js
+++ app/controller/user.js
@@ -10,18 +10,18 @@ class UserController {
             password: req.body.password,
         };
 
-        service.saveUserDetails(addressBookData, (err, data) => {
+        service.registerUser(userData, (err, data) => {
             if (err) {
                 return res.status(helper.httpStatusCodeEnum.BAD_REQUEST).json({
                     success: false,
                     message: err.message
                 });
-            } else {
-                return res.status(helper.httpStatusCodeEnum.CREATED).json({
-                    success: true,
-                    message: "User Data has been save successfully",
-                });
-            }
+            } 
+            
+            return res.status(helper.httpStatusCodeEnum.CREATED).json({
+                success: true,
+                message: "User Data has been save successfully",
+            });
         });
     }
 }
