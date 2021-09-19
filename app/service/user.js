fd0375a9bf947a8c4c7b56e91220a5dff81ad997
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-09-19 23:08:43 +0530

    refactor: added module.export class

--- app/service/user.js
+++ app/service/user.js
@@ -2,8 +2,10 @@ const model = require("../models/user");
 
 class UserService {
     registerUser = (userData, saveUserDataCallback) => {
-    model.createDetails(userData, (err, data) => {
-      return (err) ? saveUserDataCallback(err, null) : saveUserDataCallback(err, data);
-    });
-  }
+        model.createDetails(userData, (err, data) => {
+            return (err) ? saveUserDataCallback(err, null) : saveUserDataCallback(err, data);
+        });
+    }
 }
+
+module.exports = new UserService();
