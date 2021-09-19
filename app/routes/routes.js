2425d9a58e684a4175a4fa1c10a83c9825eb6b62
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-09-19 23:38:13 +0530

    refactor: added user route to save the user data and delete to delete addressbook data.

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,8 +1,13 @@
 const address = require("../controller/address");
+const user = require("../controller/user");
 
 module.exports = (app) => {
+	//User Routes
+	app.post("/saveUserData", user.saveUserData);
+
 	app.post("/saveAddressBookData", address.saveAddressBookData);
 	app.get("/getAddressBookData", address.getAddressBookData);
 	app.get("/getAddressBookData/:addressBookId", address.getAddressBookDataById);
 	app.put("/updateAddressBookData/:addressBookId", address.updateAddressBookData);
+	app.delete("/deleteAddressBookData/:addressBookId", address.deleteAddressBookDataById);
 };
