f3832dd258f0fa8fd6cd309b3822a51656c13e76
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-09-19 23:08:04 +0530

    feat: added user service to save the data

---
+++ app/service/user.js
@@ -0,0 +1,9 @@
+const model = require("../models/user");
+
+class UserService {
+    registerUser = (userData, saveUserDataCallback) => {
+    model.createDetails(userData, (err, data) => {
+      return (err) ? saveUserDataCallback(err, null) : saveUserDataCallback(err, data);
+    });
+  }
+}
