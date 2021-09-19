98ed6703d9dc14a7d3f08e8cefcc8128639d6cba
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-09-20 03:50:17 +0530

    refactor: added verify token method in routes

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,14 +1,15 @@
 const address = require("../controller/address");
 const user = require("../controller/user");
+const helper = require("../util/helper")
 
 module.exports = (app) => {
 	//User Routes
 	app.post("/saveUserData", user.saveUserData);
 	app.post("/login", user.login);
-	
-	app.post("/saveAddressBookData", address.saveAddressBookData);
-	app.get("/getAddressBookData", address.getAddressBookData);
-	app.get("/getAddressBookData/:addressBookId", address.getAddressBookDataById);
-	app.put("/updateAddressBookData/:addressBookId", address.updateAddressBookData);
-	app.delete("/deleteAddressBookData/:addressBookId", address.deleteAddressBookDataById);
+
+	app.post("/saveAddressBookData", helper.verifyToken, address.saveAddressBookData);
+	app.get("/getAddressBookData", helper.verifyToken, address.getAddressBookData);
+	app.get("/getAddressBookData/:addressBookId", helper.verifyToken, address.getAddressBookDataById);
+	app.put("/updateAddressBookData/:addressBookId",helper.verifyToken, address.updateAddressBookData);
+	app.delete("/deleteAddressBookData/:addressBookId",helper.verifyToken, address.deleteAddressBookDataById);
 };
