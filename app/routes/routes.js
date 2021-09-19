abcdde334596212777cd14ada7e0568228659b6e
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-09-20 01:14:47 +0530

    refactor: added login route

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -4,7 +4,8 @@ const user = require("../controller/user");
 module.exports = (app) => {
 	//User Routes
 	app.post("/saveUserData", user.saveUserData);
-
+	app.post("/login", user.login);
+	
 	app.post("/saveAddressBookData", address.saveAddressBookData);
 	app.get("/getAddressBookData", address.getAddressBookData);
 	app.get("/getAddressBookData/:addressBookId", address.getAddressBookDataById);
