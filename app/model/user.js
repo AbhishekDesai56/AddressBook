1fc0d4403e6a2adc8784fe05c41679e0bb46fcc6
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-09-20 01:16:48 +0530

    refactor: added login user method in model to validate user

--- app/model/user.js
+++ app/model/user.js
@@ -24,18 +24,27 @@ const userSchema =  new mongoose.Schema({
 const userModelDB = mongoose.model("user", userSchema);
 
 class UserModel {
-  createDetails = (userDetails, saveUserDataServiceCallback) => {
-    const newUser = new userModelDB({
-      firstName: userDetails.firstName,
-      lastName: userDetails.lastName,
-      email: userDetails.email,
-      password: userDetails.password,
-    });
+    createDetails = (userDetails, saveUserDataServiceCallback) => {
+        const newUser = new userModelDB({
+        firstName: userDetails.firstName,
+        lastName: userDetails.lastName,
+        email: userDetails.email,
+        password: userDetails.password,
+        });
 
-    newUser.save((error, data) => {
-      return error ? saveUserDataServiceCallback(error, null) : saveUserDataServiceCallback(null, data);
-    });
-  };
+        newUser.save((error, data) => {
+        return error ? saveUserDataServiceCallback(error, null) : saveUserDataServiceCallback(null, data);
+        });
+    }
+
+    loginUser = (loginData, authenticateUserServiceCallback) => {
+        const passwordEnteredByUser = loginData.password;
+        
+        const userCredentials  = { email: loginData.email, password: loginData.password }
+        userModelDB.findOne(userCredentials, (err, data) => {
+            return (err) ? authenticateUserServiceCallback(err, null) : authenticateUserServiceCallback(null, data);
+        });
+    }
 }
 
 module.exports = new UserModel();
