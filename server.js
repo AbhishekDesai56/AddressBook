d858a02cf78fa4286593cb9cb5addff3abaf7d3e
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-11-08 04:14:32 +0530

    refactor: added cors in server file

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
 require("dotenv").config();
 require("./config/connection");
 const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("./swagger/swagger.json")
+const swaggerDocument = require("./swagger/swagger.json");
 const logger = require("logger").createLogger("logger/development.log");
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: ["http://localhost:3001"],
+    credentials: true,
+  })
+);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-	res.json({message : "Welcome to Address Book"});
+  res.json({ message: "Welcome to Address Book" });
 });
 
 require("./app/routes/routes")(app);
 
 const server = app.listen(process.env.PORT, () => {
-	logger.info(`Server is listening on port ${process.env.PORT}`);
-	console.log(`App listening at http://localhost:${process.env.PORT}`);
+  logger.info(`Server is listening on port ${process.env.PORT}`);
+  console.log(`App listening at http://localhost:${process.env.PORT}`);
 });
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-module.exports = server
+module.exports = server;
